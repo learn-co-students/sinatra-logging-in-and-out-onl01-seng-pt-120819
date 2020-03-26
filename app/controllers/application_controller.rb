0aa211a4a65c6cfb28b2df2c1fe5a3d566996602
Mely07 <damely.tineo07@gmail.com>
2020-03-26 14:32:24 -0400

    Lab Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error
   end
+  # In the controller action that processes the POST request, you'll want to find the user in the database based on their username.
+  # If there is a match, set the session to the user's ID, redirect them to the /account route (using redirect to), and use ERB to display the user's data on the page.
+  # If there is no match, render the error page.
 
   get '/account' do
-
+    erb :account
   end
-
+  
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
 
 end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+    def self.current_user(session)
+        @user = User.find(session[:user_id])
+        @user
+    end
+
+    def self.is_logged_in?(session)
+        #!!session[:user_id]
+        session.include?(:user_id)
+    end
 end
+
+
+#These two methods will only ever be called in views, particularly account.erb, in order to add double protection to this view so that only the current user, when they are logged in, can see their bank account balance.
+#current_user should accept the session hash as an argument. This method should use the user_id from the session hash to find the user in the database and return that user.
+#is_logged_in? should also accept the session hash as an argument. This method should return true if the user_id is in the session hash and false if not. The Ruby !! operator will come in handy here.
