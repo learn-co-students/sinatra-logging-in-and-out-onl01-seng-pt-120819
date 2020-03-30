5e4804333882fd7e5636213c8217ccb6bca2d676
dwane richards <dwanerich@gmail.com>
2020-03-30 01:31:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    if @user = User.find_by(username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if !!@user.id = session[:user_id]
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
 
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ActiveRecord::Base
   
-  attr_accessor :username, :password, :balance
-  
-  def initialize(username, password, balance)
-    @username = username
-    @password = password
-    @balance = balance
-  end
-  
-  
 end
 
