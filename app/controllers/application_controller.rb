11714747944e50569e5bdaff858292754bf35d42
lkorchnoy <lkorchnoy@yahoo.com>
2020-03-29 21:23:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,20 +6,53 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  
+  get '/registrations/signup' do
+    erb :'/registration/signup' 
+  end
+
   get '/' do
     erb :index
   end
 
-  post '/login' do
-
+  post '/registration' do 
+    @user = User.new(username: params[:username], password: params[:password], balance: params[:balance])
+    #binding.pry
+    @user.save
+    session[:user_id] = @user.id
+    redirect '/account'
   end
 
-  get '/account' do
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    #binding.pry
+    if @user 
+      session[:user_id] = @user.id
+      redirect '/account'
+    #if Helpers.is_logged_in?(session) || user = Helpers.current_user(session)
+    end   
+      erb :error 
+     end
+    
 
+    get '/account' do
+      
+    @user = User.find_by(id: session[:user_id])
+    #binding.pry
+     if @user
+      erb :account
+     else
+      erb :error
+    end
   end
 
   get '/logout' do
+  session.clear
+  redirect '/'
+  end
 
+  get '/error' do 
+    erb :error
   end
 
 
